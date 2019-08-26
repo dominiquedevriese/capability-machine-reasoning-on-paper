ebbce5f57e364daeae3f3dbfa01854e1432acced
Dominique Devriese <dominique.devriese@vub.be>
2019-08-26 09:06:57 +0200

    [toplas] Add bibliography file, upload toplas-sources.zip

--- toplas/zip-sources.sh
+++ toplas/zip-sources.sh
@@ -1,2 +1,2 @@
 rm toplas-sources.zip
-zip toplas-sources.zip acmart.cls ACM-Reference-Format.bst appendix.tex paper.tex preamble-illu.tex references.bib
+zip toplas-sources.zip acmart.cls ACM-Reference-Format.bst appendix.tex paper.tex preamble-illu.tex references.bib paper.bbl
